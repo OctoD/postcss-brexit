394ad1fc91ca3cfa858b5821f450ab90e04f45b6
Paolo Roth <iamoctod@gmail.com>
2019-09-03 17:03:15 +0200

    chore: britain britain britain

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
-export function mylib(): void {}
+import postcss from "postcss";
+import { EitherLike, Left, Right } from "tiinvo";
 
-export default {
-  mylib
-};
+export interface IBrexitOptions {
+  noDeal?: boolean;
+}
+
+interface IPropertiesDictionary {}
+
+function shouldCheck(
+  property: string,
+  propertiesDictionary: IPropertiesDictionary
+): EitherLike<boolean, boolean> {
+  return property in propertiesDictionary ? Left(false) : Right(true);
+}
+
+function walkDeclarations(decl: postcss.Declaration, index: number): any {}
+
+module.exports = postcss.plugin<IBrexitOptions>("🇬🇧brexit🇬🇧", options => {
+  return (root, result) => {
+    root.walkDecls("*", walkDeclarations);
+  };
+});
