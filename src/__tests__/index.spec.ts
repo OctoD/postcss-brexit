f7b714c58ea40f4d4de170f0080a0a76074ddd1e
Paolo Roth <iamoctod@gmail.com>
2019-09-04 14:31:19 +0200

    chore: again mates

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,7 +1,22 @@
+import postcss from "postcss";
+import brexit from "../index";
+
 describe(`mylib`, () => {
   //#region placeholder
-  it(`is a placeholder`, () => {
-    expect(1).toBe(1);
+  it(`Tests a soft brexit scenario`, async () => {
+    const css = `
+      .brexit {
+        background-colour: white;
+        colour: blue;
+      }
+    `;
+
+    expect(
+      async () => await postcss([brexit()]).process(css)
+    ).not.toThrowError();
+    const result = await postcss([brexit()]).process(css);
+
+    expect(result.messages.length).toBe(0);
   });
   //#endregion
 });
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,87 @@
 import postcss from "postcss";
-import { EitherLike, Left, Right } from "tiinvo";
+import { Maybe } from "tiinvo";
 
 export interface IBrexitOptions {
   noDeal?: boolean;
 }
 
+function createHardBrexitHandler(root: postcss.Root) {
+  return () => handleHardBrexit(root);
+}
+
+function createSoftBrexitHandler(root: postcss.Root) {
+  return () => handleSoftBrexit(root);
+}
+
+function handle(root: postcss.Root) {
+  walkDecl(root, "background-colour", "background-color");
+  walkDecl(root, "colour", "color");
+}
+
+function handleHardBrexit(root: postcss.Root) {
+  walkDeclAndFail(root, "background-color");
+  walkDeclAndFail(root, "color");
+  handle(root);
+}
+
+function handleSoftBrexit(root: postcss.Root) {
+  walkDeclAndWarn(root, "background-color");
+  walkDeclAndWarn(root, "color");
+  handle(root);
+}
+
+function replaceDecl(decl: postcss.Declaration, nextPropertyName: string) {
+  decl.replaceWith(
+    postcss.decl({
+      prop: nextPropertyName,
+      value: decl.value
+    })
+  );
+}
+
 function replaceFactory(
   nextPropertyName: string
 ): (decl: postcss.Declaration) => any {
-  return decl => {
-    decl.replaceWith(
-      postcss.decl({ prop: nextPropertyName, value: decl.value })
-    );
-  };
+  return decl => replaceDecl(decl, nextPropertyName);
 }
 
-module.exports = postcss.plugin<IBrexitOptions>("🇬🇧brexit🇬🇧", options => {
-  return root => {
-    root.walkDecls("background-colour", replaceFactory("background-color"));
-    root.walkDecls("colour", replaceFactory("color"));
-  };
+function walkDecl(
+  root: postcss.Root,
+  previousCssProperty: string,
+  replacementCssProperty: string
+) {
+  root.walkDecls(previousCssProperty, replaceFactory(replacementCssProperty));
+}
+
+function walkDeclAndFail(root: postcss.Root, cssproperty: string): void {
+  root.walkDecls(cssproperty, decl => {
+    const error = new Error();
+
+    error.name = "🇬🇧Britain, Britain, Britain🇬🇧";
+    error.message = `🇬🇧Warning, American English Detected🇬🇧`;
+
+    throw error;
+  });
+}
+
+function walkDeclAndWarn(root: postcss.Root, cssproperty: string): void {
+  root.walkDecls(cssproperty, decl => {
+    console.warn(`🇬🇧Warning, American English Detected🇬🇧`);
+  });
+}
+
+const brexitplugin = postcss.plugin<IBrexitOptions>("🇬🇧brexit🇬🇧", options => {
+  return root =>
+    Maybe(options).cata({
+      Nothing: createSoftBrexitHandler(root),
+      Just: (val: IBrexitOptions) =>
+        Maybe(val.noDeal).cata({
+          Nothing: createSoftBrexitHandler(root),
+          Just: createHardBrexitHandler(root)
+        })
+    });
 });
+
+module.exports = brexitplugin;
+
+export default brexitplugin;
